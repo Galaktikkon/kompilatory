57ef932b88e89a8e8efaa24505f8e2ac53611314
joannakonieczny <konieczny.joasia@gmail.com>
2024-10-11 18:15:45 +0200

    Add tokens to SEMICOLON

--- lab1/main.py
+++ lab1/main.py
@@ -3,7 +3,6 @@ from sly import Lexer
 
 
 class Scanner(Lexer):
-
     tokens = {ID, PLUS, MINUS, MUL, DIV, MAT_PLUS,
               MAT_MINUS, MAT_MUL, MAT_DIV, ASSIGN,
               ADD_ASSIGN, SUB_ASSIGN, MUL_ASSIGN,
@@ -12,21 +11,51 @@ class Scanner(Lexer):
               CL_SQ_BRAC, OP_CRL_BRAC, CL_CRL_BRAC,
               RANGE, TRANSPOSE, COMMA, SEMICOLON, BREAK,
               CONTINUE, RETURN, EYE, ZEROS, ONES, PRINT,
-              INT, FLOAT, STR
+              FLOAT, INT, STR
               }
 
     ignore = ' \t'
+    ignore_comment = r'\#.*'
+    ignore_newline = r'\n+'
 
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
     PLUS = r'\+'
-    MINUS = r'\-'
+    MINUS = r'-'
     MUL = r'\*'
-    DIV = r'\/'
+    DIV = r'/'
 
-    ID['\.+'] = MAT_PLUS
-    ID['\.-'] = MAT_MINUS
-    ID['\.*'] = MAT_MUL
-    ID['\./'] = MAT_DIV
+    MAT_PLUS = r'.\+'
+    MAT_MINUS = r'.-'
+    MAT_MUL = r'.\*'
+    MAT_DIV = r'./'
+
+    ASSIGN = r'='
+    ADD_ASSIGN = r'\+='
+    SUB_ASSIGN = r'-='
+    MUL_ASSIGN = r'\*='
+    DIV_ASSIGN = r'/='
+
+    LESS = r'<'
+    GREATER = r'>'
+    LEQ = r'<='
+    GEQ = r'>='
+    NEQ = r'!='
+    EQ = r'=='
+
+    OP_BRAC = r'\('
+    CL_BRAC = r'\)'
+    OP_SQ_BRAC = r'\['
+    CL_SQ_BRAC = r'\]'
+    OP_CRL_BRAC = r'\{'
+    CL_CRL_BRAC = r'\}'
+    RANGE = r':'
+    TRANSPOSE = r'\''
+    COMMA = r','
+    SEMICOLON = ';'
+
+    INT = r'\d+'
+    
+
 
 
 if __name__ == '__main__':
