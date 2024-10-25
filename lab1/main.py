a6ef50e455248e2d4b609337561d69528db12aca
Marek Malek <malekmarek56@gmail.com>
2024-10-25 17:51:11 +0200

    update regex string

--- lab1/main.py
+++ lab1/main.py
@@ -6,95 +6,120 @@ class Scanner(Lexer):
 
     # Reserved keywords
     reserved = {
-        'if': 'IF',
-        'else': 'ELSE',
-        'for': 'FOR',
-        'while': 'WHILE',
-        'break': 'BREAK',
-        'continue': 'CONTINUE',
-        'return': 'RETURN',
-        'eye': 'EYE',
-        'zeros': 'ZEROS',
-        'ones': 'ONES',
-        'print': 'PRINT'
+        "if": "IF",
+        "else": "ELSE",
+        "for": "FOR",
+        "while": "WHILE",
+        "break": "BREAK",
+        "continue": "CONTINUE",
+        "return": "RETURN",
+        "eye": "EYE",
+        "zeros": "ZEROS",
+        "ones": "ONES",
+        "print": "PRINT",
     }
 
     tokens = {
-        ID, MAT_PLUS, MAT_MINUS, MAT_MUL, MAT_DIV,
-        ADD_ASSIGN, SUB_ASSIGN, MUL_ASSIGN,
-        DIV_ASSIGN, LEQ, GEQ, EQ, NEQ, FLOAT, INT, STR
+        ID,
+        MAT_PLUS,
+        MAT_MINUS,
+        MAT_MUL,
+        MAT_DIV,
+        ADD_ASSIGN,
+        SUB_ASSIGN,
+        MUL_ASSIGN,
+        DIV_ASSIGN,
+        LEQ,
+        GEQ,
+        EQ,
+        NEQ,
+        FLOAT,
+        INT,
+        STR,
     } | set(reserved.values())
 
     # Literals
     literals = {
-        '+', '-', '*', '/',
-        '=', '>', '<', '(',
-        ')', '[', ']', '{',
-        '}', ',', ':', ';',
-        '\''
+        "+",
+        "-",
+        "*",
+        "/",
+        "=",
+        ">",
+        "<",
+        "(",
+        ")",
+        "[",
+        "]",
+        "{",
+        "}",
+        ",",
+        ":",
+        ";",
+        "'",
     }
 
     # String containing ignored characters (between tokens)
-    ignore = ' \t'
+    ignore = " \t"
 
     # Ignore comments
-    ignore_comment = r'\#.*'
+    ignore_comment = r"\#.*"
 
     # Define a rule so we can track line numbers
-    @_(r'\n+')
+    @_(r"\n+")
     def ignore_newline(self, t):
         self.lineno += len(t.value)
 
     # Base ID rule
-    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID = r"[a-zA-Z_][a-zA-Z0-9_]*"
 
     # Special cases
-    ID['if'] = IF
-    ID['else'] = ELSE
-    ID['for'] = FOR
-    ID['while'] = WHILE
-    ID['break'] = BREAK
-    ID['continue'] = CONTINUE
-    ID['return'] = RETURN
-    ID['eye'] = EYE
-    ID['zeros'] = ZEROS
-    ID['ones'] = ONES
-    ID['print'] = PRINT
+    ID["if"] = IF
+    ID["else"] = ELSE
+    ID["for"] = FOR
+    ID["while"] = WHILE
+    ID["break"] = BREAK
+    ID["continue"] = CONTINUE
+    ID["return"] = RETURN
+    ID["eye"] = EYE
+    ID["zeros"] = ZEROS
+    ID["ones"] = ONES
+    ID["print"] = PRINT
 
     # Matrix operators
-    MAT_PLUS = r'.\+'
-    MAT_MINUS = r'.-'
-    MAT_MUL = r'.\*'
-    MAT_DIV = r'./'
+    MAT_PLUS = r".\+"
+    MAT_MINUS = r".-"
+    MAT_MUL = r".\*"
+    MAT_DIV = r"./"
 
     # Assign operators
-    ADD_ASSIGN = r'\+='
-    SUB_ASSIGN = r'-='
-    MUL_ASSIGN = r'\*='
-    DIV_ASSIGN = r'/='
+    ADD_ASSIGN = r"\+="
+    SUB_ASSIGN = r"-="
+    MUL_ASSIGN = r"\*="
+    DIV_ASSIGN = r"/="
 
     # Comparison operators
-    LEQ = r'<='
-    GEQ = r'>='
-    EQ = r'=='
-    NEQ = r'!='
+    LEQ = r"<="
+    GEQ = r">="
+    EQ = r"=="
+    NEQ = r"!="
 
     # Strings
-    STR = r'"([^"\\]*(?:\\.[^"\\]*)*)?"'
+    STR = r'"([.*?\\]*(?:\\.[\\]*)*)?"'
 
     # Floats
-    FLOAT = r'\.\d+([eE][+-]?\d+)?|\d+\.\d+([eE][+-]?\d+)?|\d+\.'
+    FLOAT = r"\.\d+([eE][+-]?\d+)?|\d+\.\d*([eE][+-]?\d+)?|\d+([eE][+-]?\d+)?"
 
     # Integers
-    INT = r'\d+'
+    INT = r"\d+"
 
     # Error handling rule for illegal characters
     def error(self, t):
-        print(f'({self.lineno}): !!! Illegal character: \'{t.value[0]}\' !!!')
+        print(f"({self.lineno}): !!! Illegal character: '{t.value[0]}' !!!")
         self.index += 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     try:
         filename = sys.argv[1] if len(sys.argv) > 1 else "examples\example.txt"
@@ -107,6 +132,4 @@ if __name__ == '__main__':
     lexer = Scanner()
 
     for tok in lexer.tokenize(text):
-        print(
-            f'({tok.lineno}): {tok.type} (\'{tok.value}\')'
-        )
+        print(f"({tok.lineno}): {tok.type} ('{tok.value}')")
