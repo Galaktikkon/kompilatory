1600e4c3aa6cc3a9cbabda597249d4354b59ea83
joannakonieczny <konieczny.joasia@gmail.com>
2024-11-07 19:03:28 +0100

    First fixes

--- lab2/parser.py
+++ lab2/parser.py
@@ -12,67 +12,112 @@ class Mparser(Parser):
 
 
     precedence = (
+        ("left", ","),
+        ('left', "+", "-", MAT_PLUS, MAT_MINUS),
+        ('left', "*", "/",  MAT_MUL, MAT_DIV),
+        ('right', UMINUS), 
+        ("right", ELSE),
     )
 
-
-    @_('body line')
+    @_('lines')
     def body(self, p):
+        pass
+
+    @_('lines line')
+    def lines(self, p):
         pass
     
     @_('line')
-    def body(self, p):
+    def lines(self, p):
         pass
 
-    @_('PRINT expr ";"')
+    @_('print_statement ";"')
     def line(self, p):
         pass
 
-    @_('ID "=" expr ";"',
-       'ID ADD_ASSIGN expr ";"',
-       'ID SUB_ASSIGN expr ";"',
-       'ID MUL_ASSIGN expr ";"',
-       'ID DIV_ASSIGN expr ";"')
+    @_('PRINT expr')
+    def print_statement(self, p):
+        pass
+    
+    @_('assignment ";"')
+    def line(self, p):
+        pass
+    
+    @_('ID "=" expr',
+       'ID ADD_ASSIGN expr',
+       'ID SUB_ASSIGN expr',
+       'ID MUL_ASSIGN expr',
+       'ID DIV_ASSIGN expr')
+    def assignment(self, p):
+        pass
+
+    @_('ID "[" factor "]" "=" expr')
+    def assignment(self, p):
+        pass
+
+    @_('control_statement ";"')
     def line(self, p):
         pass
 
-    @_('BREAK ";"',
-       'CONTINUE ";"')
+    @_('BREAK',
+       'CONTINUE')
+    def control_statement(self, p):
+        pass
+
+    @_('return_statement ";"')
     def line(self, p):
         pass
 
-    @_('RETURN expr ";"')
-    def line(self, p):
+    @_('RETURN expr')
+    def return_statement(self, p):
         pass
 
     @_('"{" body "}"')
     def line(self, p):
         pass
 
-    @_('IF condition body',
-       'WHILE condition body')
+    @_('if_statement')
     def line(self, p):
         pass
 
-    @_('ELSE body')
-    def line(self, p):
+    @_('IF condition "{" body "}" ELSE "{" body "}" ',
+       'IF condition "{" body "}" %prec ELSE')
+    def if_statement(self, p):
         pass
 
-    @_('"(" expr EQ expr ")"',
-       '"(" expr NEQ expr ")"',
-       '"(" expr LEQ expr ")"',
-       '"(" expr GEQ expr ")"',
-       '"(" expr "<" expr ")"',
-       '"(" expr ">" expr ")"')
+    @_('"(" statement ")"')
     def condition(self, p):
         pass
 
-    @_('FOR ID "=" element ":" element body')
+    @_('expr EQ expr',
+       'expr NEQ expr',
+       'expr LEQ expr',
+       'expr GEQ expr',
+       'expr "<" expr',
+       'expr ">" expr')
+    def statement(self, p):
+        pass
+
+    @_('ID factor EQ expr',
+       'ID factor NEQ expr')
+    def statement(self, p):
+        pass
+
+    @_('loop')
     def line(self, p):
+        pass
+
+    @_('FOR ID "=" enumerable ":" enumerable "{" body "}"',
+       'WHILE condition "{" body "}"')
+    def loop(self, p):
         pass
     
     @_('expr "+" term',
-       'expr "-" term',
-       'expr MAT_PLUS term',
+       'expr "-" term')
+    def expr(self, p):
+        pass
+
+    @_('expr MAT_PLUS term',
        'expr MAT_MINUS term')
     def expr(self, p):
         pass
@@ -83,13 +128,16 @@ class Mparser(Parser):
         pass
 
     @_('term "*" factor',
-       'term "/" factor',
-       'term MAT_MUL factor',
+       'term "/" factor')
+    def term(self, p):
+        pass
+
+    @_('term MAT_MUL factor',
        'term MAT_DIV factor')
     def term(self, p):
         pass
 
-    @_('factor')
+    @_('factor %prec ","')
     def term(self, p):
         # return p.factor
         pass
@@ -102,20 +150,25 @@ class Mparser(Parser):
     def factor(self, p):
         pass
 
-    @_('ZEROS "(" INT ")"', 
-       'EYE "(" INT ")"',
-       'ONES "(" INT ")"')
+    @_('ZEROS "(" enumerable ")"', 
+       'EYE "(" enumerable ")"',
+       'ONES "(" enumerable ")"')
     def element(self, p):
         pass
 
-    @_('FLOAT',
-       'INT',
-       'STR',
-       'ID')
+    @_('STR')
     def element(self, p):
         pass
 
-    @_('"[" element "]"')
+    @_('enumerable')
+    def element(self, p):
+        pass
+
+    @_('FLOAT', 'INT', 'ID')
+    def enumerable(self, p):
+        pass
+
+    @_('ID "\'" ')
     def element(self, p):
         pass
 
@@ -126,4 +179,7 @@ class Mparser(Parser):
     @_('"(" expr ")"')
     def element(self, p):
         pass
-    
+
+    @_('"-" expr %prec UMINUS')
+    def element(self, p):
+        pass
